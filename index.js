65e2e8e30f4f5e76bf8406229c66804acad190e4
RyanTaylorRVAMass <ryantaylor@resortscompanies.com>
2024-12-30 17:24:25 -0500

    pull users and tours from stored procedures

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const https = require('https');
+const http = require('http');
 const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
+const { connectToDatabase } = require('./dbConfig');
 require('dotenv').config({ path: '.env.local' });
 
-const { fetchAuthToken } = require('./service');
+const { fetchAuthToken, callSoapAPI, getUserList, getTourList } = require('./service');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post('/fetchAuthToken', async (req, res) => { // Fixed parameter signature
+app.post('/fetchAuthToken', async (req, res) => {
   try {
     const data = await fetchAuthToken();
     res.json(data);
@@ -20,11 +22,58 @@ app.post('/fetchAuthToken', async (req, res) => { // Fixed parameter signature
   }
 });
 
-const privateKey = fs.readFileSync('C:/Users/ryantaylor/.vscode/projects/Affinity/certs/privateKey.pem', 'utf8');
-const certificate = fs.readFileSync('C:/Users/ryantaylor/.vscode/projects/Affinity/certs/certificate.pem', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
+app.post('/callSoapAPI', async (req, res) => {
+  try {
+    const data = await callSoapAPI(req.body);
+    res.json(data);
+  } catch (error) {
+    console.error('Error in /callSoapAPI route:', error.message);
+    res.status(500).json({ error: error.message || 'Failed to process request' });
+  }
+});
 
-const httpsServer = https.createServer(credentials, app);
-httpsServer.listen(3001, '0.0.0.0', () => {
-  console.log('HTTPS Server running on port 3001');
+app.get('/getUserList', async (req, res) => {
+  try {
+    const data = await getUserList();
+    res.json(data);
+  } catch (err) {
+    console.error('Error in /getUserList route:', err.message);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+app.post('/getTourList', async (req, res) => {
+  try {
+    const { username, date, locationID } = req.body;
+
+    if (!username || !date || !locationID) {
+      return res.status(400).json({
+        error: "Please provide 'username', 'date', and 'locationID' in the request body.",
+      });
+    }
+
+    const data = await getTourList(username, date, locationID);
+    res.json(data);
+  } catch (err) {
+    console.error('Error in /getTourList route:', err.message);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+// const privateKey = fs.readFileSync('C:/Users/ryantaylor/.vscode/projects/Affinity/certs/privateKey.pem', 'utf8');
+// const certificate = fs.readFileSync('C:/Users/ryantaylor/.vscode/projects/Affinity/certs/certificate.pem', 'utf8');
+// const credentials = { key: privateKey, cert: certificate };
+
+// connectToDatabase().then(() => {
+//   const httpsServer = https.createServer(credentials, app);
+//   httpsServer.listen(3001, '0.0.0.0', () => {
+//     console.log('HTTPS Server running on port 3001');
+//   });
+// });
+
+connectToDatabase().then(() => {
+  const httpServer = http.createServer(app);
+  httpServer.listen(3001, '0.0.0.0', () => {
+    console.log('HTTP Server running on port 3001');
+  });
 });
--- service.js
+++ service.js
@@ -1,8 +1,10 @@
+const { parseStringPromise } = require('xml2js');
 const axios = require('axios');
 const { wrapper } = require('axios-cookiejar-support');
 const tough = require('tough-cookie');
+const { connectToDatabase, sql } = require('./dbConfig');
 
-const apiURL = process.env.URL
+const apiURL = process.env.URL;
 
 const fetchAuthToken = async () => {
   try {
@@ -10,7 +12,6 @@ const fetchAuthToken = async () => {
     const client = wrapper(axios.create({ jar }));
 
     const url = `${apiURL}/oauth/oauth20/token`;
-    console.log(url)
     const headers = {
       'Content-Type': 'application/x-www-form-urlencoded',
     };
@@ -22,15 +23,83 @@ const fetchAuthToken = async () => {
     });
 
     const response = await client.post(url, data, { headers });
-
     console.log('Access Token:', response.data);
-    return response.data;
+    return response.data.access_token;
   } catch (error) {
     console.error('Error fetching token:', error.response?.data || error.message);
     throw error;
   }
 };
 
+const callSoapAPI = async (data) => {
+  try {
+    const accessToken = await fetchAuthToken();
+    const soapUrl = `${apiURL}/FSVRemote/v1/fsvremote`;
+    const headers = {
+      'Content-Type': 'text/xml; charset=utf-8',
+      'SOAPAction': 'http://FSVWebServices/FSVRemote/Transact',
+      Authorization: `Bearer ${accessToken}`,
+    };
+
+    const soapBody = `
+        <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+          <soap:Body>
+            <Transact xmlns="http://FSVWebServices/FSVRemote/">
+              <userid>${process.env.USER_ID}</userid>
+              <password>${process.env.PASSWORD}</password>
+              <paramstr><![CDATA[${data.certCard},${data.passcode},${data.transaction},${data.transactionString}]]></paramstr>
+            </Transact>
+          </soap:Body>
+        </soap:Envelope>
+      `;
+
+    const response = await axios.post(soapUrl, soapBody, { headers });
+
+    const parsed = await parseStringPromise(response.data, { trim: true, explicitArray: false });
+
+    const result = parsed['soap:Envelope']['soap:Body']['TransactResponse']['TransactResult'];
+    return result;
+  } catch (error) {
+    console.error('Error in SOAP call:', error.response?.data || error.message);
+    throw error;
+  }
+};
+
+const getUserList = async () => {
+  try {
+    await connectToDatabase();
+
+    const storedProcedure = 'GetUserList';
+    const request = new sql.Request();
+
+    const result = await request.execute(storedProcedure);
+    return result.recordset;
+  } catch (err) {
+    console.error('Error executing stored procedure:', err.message);
+    throw err;
+  }
+};
+
+const getTourList = async (username, date, locationID) => {
+  try {
+    await connectToDatabase();
+
+    const request = new sql.Request();
+    request.input('UserName', sql.NVarChar(50), username);
+    request.input('Date', sql.DateTime, date);
+    request.input('LocationID', sql.Int, locationID);
+
+    const result = await request.execute('GetUserTourList_SS');
+    return result.recordset;
+  } catch (err) {
+    console.error('Error executing GetUserTourList_SS:', err.message);
+    throw err;
+  }
+};
+
 module.exports = {
   fetchAuthToken,
+  callSoapAPI,
+  getUserList,
+  getTourList,
 };
